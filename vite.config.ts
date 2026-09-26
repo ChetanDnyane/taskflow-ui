a8f80eeaef804731a5d1e93c9cb96d0db934c551
Chetan Valmik Dnyane <dnyane.chetan@gmail.com>
2026-09-26 10:48:50 +0530

    Updated to talk to multiple microservices

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   const proxy = {
-    '/api': { target: env.API_PROXY_TARGET || 'http://127.0.0.1:8080', changeOrigin: true },
-  };
+  '/api/auth': {
+    target: env.AUTH_API_PROXY_TARGET || 'http://127.0.0.1:8080',
+    changeOrigin: true,
+  },
+
+  '/api/tasks': {
+    target: env.TASK_API_PROXY_TARGET || 'http://127.0.0.1:8081',
+    changeOrigin: true,
+  },
+};
   return {
     plugins: [react()],
     server: { port: 5173, strictPort: true, proxy },
